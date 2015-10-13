8ca4f674e909c4c288fb3e89307060a64724335a
Glen Pierce <glen.piercev@gmail.com>
2015-10-12 21:23:14 -0700

    EditText in main_activity isn't responding to methods dedicated to adding new layouts.

--- app/src/main/java/piercestudio/com/listmaker/MainActivity.java
+++ app/src/main/java/piercestudio/com/listmaker/MainActivity.java
@@ -1,26 +1,30 @@
 package piercestudio.com.listmaker;
 
 import android.app.Activity;
-import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.view.ViewGroup.LayoutParams;
 import android.util.Log;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
-import android.view.ViewParent;
 
 
 public class MainActivity extends Activity
 {
+	String TAG = "buttonTestTag";
 
-	LinearLayout listLayout;
-	int buttonCount;
-	float onDownX;
+	private LinearLayout linearLayoutInsideScrollView;
+	private EditText editText;
+	int buttonCount = 0;
 
 
 	@Override
@@ -30,163 +34,75 @@ public class MainActivity extends Activity
 		getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		listLayout = (LinearLayout) findViewById(R.id.listLayout);
 
-		TextView createButton = new TextView(this);
-		createButton = (TextView) listLayout.findViewWithTag("1");
-		createButton.setOnClickListener(addButton);
+		LayoutInflater inflater = MainActivity.this.getLayoutInflater();
+		View layoutView = inflater.inflate(R.layout.activity_main, null);
 
-		buttonCount = 1;
+		linearLayoutInsideScrollView = new LinearLayout(getApplication());
+		linearLayoutInsideScrollView = (LinearLayout) layoutView.findViewById(R.id.layoutInsideScrollView);
+
+		editText = new EditText(this);
+		editText = (EditText) layoutView.findViewById(R.id.editText);
+		editText.setText("asdf");
+
+		Button addNewToDo = (Button) findViewById(R.id.addNewToDoButon);
+		OnClickListener toDoOnClickListner = createNewToDoOnClickListner();
+		addNewToDo.setOnClickListener(toDoOnClickListner);
+
 	}
 
-	OnClickListener addButton = new OnClickListener()
-	{
-
-		public void onClick(View v)
-		{
-			buttonCount++;
-
-			LinearLayout newLinearLayout;
-			newLinearLayout = new LinearLayout(getApplicationContext());
-			LayoutInflater inflater = MainActivity.this.getLayoutInflater();
-			View layoutView = inflater.inflate(R.layout.new_linear_layout, null);
-			newLinearLayout = (LinearLayout) layoutView.findViewById(R.id.newlinearlayout);
-
-			TextView textView = (TextView) layoutView.findViewById(R.id.textview);
-			textView.setTag(Integer.toString(buttonCount));
-
-			newLinearLayout.setTag(Integer.toString(buttonCount));
-			Log.i("asdf", newLinearLayout.getTag().toString());
-			newLinearLayout.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
-
-			setTextViewText(textView);
-
-			listLayout.addView(newLinearLayout);
-
-
-//			newLinearLayout.setOnTouchListener(onTouchListener);
-			textView.setOnTouchListener(onTouchListenerHeadings);
-		}
-
-	};
-
-	View.OnTouchListener onTouchListener = new View.OnTouchListener()
-	{
-		@Override
-		public boolean onTouch(View v, MotionEvent event)
-		{
-
-			Long downTime = 0L;
-			int maskedAction = event.getActionMasked();
-
-			switch (maskedAction)
-			{
-				case MotionEvent.ACTION_DOWN:
-				case MotionEvent.ACTION_POINTER_DOWN:
-				{
-					Log.i("asdf", "down");
-					onDownX = event.getX();
-					downTime = System.currentTimeMillis();
-					break;
-				}
-
-				case MotionEvent.ACTION_UP:
-				case MotionEvent.ACTION_POINTER_UP:
-				{
-					Log.i("asdf", "up");
-					if (event.getX() - onDownX > 200){
-						((ViewGroup)v.getParent()).removeView(v);
-						break;
-					}
-					if(System.currentTimeMillis() - downTime > 200){
-//						addNewItem((LinearLayout) v);
-						addNewItemFromView(v);
-					}
-
-					break;
-				}
-			}
-			return true;
-		}
-	};
-
-	View.OnTouchListener onTouchListenerHeadings = new View.OnTouchListener()
-	{
-		@Override
-		public boolean onTouch(View v, MotionEvent event)
-		{
-
-			Long downTime = 0L;
-			int maskedAction = event.getActionMasked();
-
-			switch (maskedAction)
-			{
-				case MotionEvent.ACTION_DOWN:
-				case MotionEvent.ACTION_POINTER_DOWN:
-				{
-					Log.i("asdf", "down");
-					onDownX = event.getX();
-					downTime = System.currentTimeMillis();
-					break;
-				}
-
-				case MotionEvent.ACTION_UP:
-				case MotionEvent.ACTION_POINTER_UP:
-				{
-					Log.i("asdf", "up");
-					if (event.getX() - onDownX > 200){
-						Log.i("asdf", "deleting header" + v.getTag());
-						Log.i("asdf", ((ViewGroup) v.getParent()).findViewWithTag(v.getTag()).getTag().toString());
-						((ViewGroup)v.getParent().getParent()).removeView((ViewGroup)v.getParent());
-						break;
-					}
-					if(System.currentTimeMillis() - downTime > 200)
-					{
-//						addNewItem((LinearLayout) v);
-						addNewItemFromView(v);
-					}
-
-					break;
-				}
-			}
-			return true;
-		}
-	};
-
-//	public void addNewItem(LinearLayout v){
-//		TextView newTextView;
-//		newTextView = new TextView(getApplicationContext());
-//		LayoutInflater inflater = MainActivity.this.getLayoutInflater();
-//		View layoutView = inflater.inflate(R.layout.new_list_item, null);
-//		TextView textView = (TextView) layoutView.findViewById(R.id.new_item);
-//
-//		setTextViewText(textView);
-//
-//		v.addView(layoutView);
-//		textView.setOnTouchListener(onTouchListener);
+//	public void createKeyListners(){
+//		View.OnKeyListener addToDo = new View.OnKeyListener()
+//		{
+//			@Override
+//			public boolean onKey(View v, int keyCode, KeyEvent event)
+//			{
+//				if(event.getAction() == KeyEvent.ACTION_DOWN && event.getKeyCode() == KeyEvent.KEYCODE_ENTER){
+//					createNewToDo();
+//				}
+//				return false;
+//			}
+//		};
 //	}
 
-	public void addNewItemFromView(View v){
-		TextView newTextView;
-		newTextView = new TextView(getApplicationContext());
+
+	public void createNewToDo()
+	{
+		editText.setText("");
+		Log.i(TAG, "Click");
+		buttonCount++;
+		Log.i(TAG, "Click! " + Integer.toString(buttonCount) + editText.getText().toString());
+
 		LayoutInflater inflater = MainActivity.this.getLayoutInflater();
-		View layoutView = inflater.inflate(R.layout.new_item_layout, null);
-		TextView textView = (TextView) layoutView.findViewById(R.id.new_item_textview);
-		LinearLayout newItem = (LinearLayout) layoutView.findViewById(R.id.new_item);
+		View layoutView = inflater.inflate(R.layout.layout_to_be_added_with_new_textview, null);
 
-		setTextViewText(textView);
+		LinearLayout linearLayoutToBeAddedFromEditText;
+		linearLayoutToBeAddedFromEditText = new LinearLayout(getApplicationContext());
+		linearLayoutToBeAddedFromEditText.setTag(Integer.toString(buttonCount));
+		linearLayoutToBeAddedFromEditText.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
 
-		((ViewGroup)v.getParent()).addView(layoutView);
+		TextView newToDo;
+		newToDo = new TextView(getApplicationContext());
+		newToDo = (TextView) layoutView.findViewById(R.id.newToDoTextView);
+		newToDo.setTag(Integer.toString(buttonCount));
 
-		layoutView.setOnTouchListener(onTouchListener);
+		newToDo.setText(editText.getText().toString());
+		editText.setText("");
+
+		linearLayoutInsideScrollView.addView(layoutView);
 	}
 
-	public void setTextViewText(TextView textView){
-		FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
-		UserEntryDialogFragment userEntryDialogFragment = new UserEntryDialogFragment();
-		userEntryDialogFragment.initialize(textView);
 
-		userEntryDialogFragment.show(fragmentTransaction, getString(R.string.userEntryDialogFragmentTag));
-		userEntryDialogFragment.setTextView(textView);
+
+	public OnClickListener createNewToDoOnClickListner(){
+		OnClickListener newOnClickListner = new OnClickListener()
+		{
+			@Override
+			public void onClick(View v)
+			{
+				createNewToDo();
+			}
+		};
+		return newOnClickListner;
 	}
 }
